74ebf94caf3c5b485d41f6a54f8afe0573ac36cd
Aaron Cruces <aaroncruces@gmail.com>
2025-07-20 04:01:17 -0400

    update

--- system/static/users.nix
+++ system/static/users.nix
@@ -5,10 +5,5 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ]; # sudo and NetworkManager access
     initialPassword = "changeme"; # Change after install
-    initialFiles.".profile".text = ''
-      if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
-        exec startx
-      fi
-    '';
   };
 }
