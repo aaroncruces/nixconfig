3e03beb87adb4d02537e633493738fee9822d8dd
Aaron Cruces <aaroncruces@gmail.com>
2025-07-20 03:49:20 -0400

    update

--- initial_commands.sh
+++ initial_commands.sh
@@ -1,5 +1,6 @@
 nix-env -iA nixos.tmux
 nix-env -iA nixos.htop
+nix-env -iA nixos.git
 tmux
 rm install.sh -vf && nano install.sh && bash install.sh -d sda && less /mnt/etc/nixos/configuration.nix
 nixos-install
