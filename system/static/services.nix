cf2ca954dacaaf62f1a9d63fd65b61b860a630dd
Aaron Cruces <aaroncruces@gmail.com>
2025-07-20 04:46:36 -0400

    updates

--- system/static/services.nix
+++ system/static/services.nix
@@ -9,4 +9,7 @@
 
   # Explicitly disable display managers
   services.displayManager.sddm.enable = false;
+  services.xserver.displayManager.sddm.enable = false;
+  services.xserver.displayManager.lightdm.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
 }
