fa51461af68982531f2459836117bf797d55a6d9
Aaron Cruces <aaroncruces@gmail.com>
2025-07-20 20:07:36 -0400

    test

--- user/homemanager.nix
+++ user/homemanager.nix
@@ -1,21 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports =
-    [
-      (import "${home-manager}/nixos")
-    ];
 
   # Add home-manager to system packages
   environment.systemPackages = with pkgs; [
     home-manager
   ];
-  home-manager.users.aaron = { pkgs, ... }: {
-    home.packages = [ pkgs.btop pkgs.httpie ];
-    programs.bash.enable = true;
-  
-    # The state version is required and should stay at the version you
-    # originally installed.
-    home.stateVersion = "25.05";
-  };
+
 }
