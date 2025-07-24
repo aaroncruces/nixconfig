d6459ccef9a72293c571d71de6f5c8cc62aa00ba
Aaron Cruces <aaroncruces@gmail.com>
2025-07-23 20:28:13 -0400

    nvidia

--- system/static/nvidia.nix
+++ system/static/nvidia.nix
@@ -4,37 +4,27 @@
   # Enable the X11 windowing system and Hyprland
   services.xserver = {
     enable = true;
+    videoDrivers = [ "nvidia" ];
     exportConfiguration = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = false; # Optional: disable GNOME if not needed
   };
 
-  # NVIDIA drivers
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-
-  # Ensure OpenGL is enabled for NVIDIA
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+  hardware = {
+    opengl.enable =  true;
+    nvidia = {
+      modesetting.enable = true; # Required for Wayland
+      powerManagement.enable = false; # May cause issues with some setups, adjust as needed
+      package = config.boot.kernelPackages.nvidiaPackages.stable; # Use stable NVIDIA drivers
+    };
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
   };
-
-  # NVIDIA hardware configuration
-  hardware.nvidia = {
-    modesetting.enable = true; # Required for Wayland
-    powerManagement.enable = false; # May cause issues with some setups, adjust as needed
-    package = config.boot.kernelPackages.nvidiaPackages.stable; # Use stable NVIDIA drivers
-  };
-
-  # GPU acceleration
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-
-  ########## NVIDIA ##########
-  programs.hyprland.nvidiaPatches = true; # Enable NVIDIA-specific patches
+  
+  environment.systemPackages = with pkgs; [
+    (ffmpeg.override { withNvenc = true; }) # Enable NVENC for FFmpeg
+  ];
 
 }
